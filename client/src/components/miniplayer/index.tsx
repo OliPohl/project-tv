012e64a75ceabb2e0b6356f0fd525a8e43cea8ce
OliPohl <oliver@pohl66.de>
2025-05-04 18:24:15 +0200

    base icons in the miniplayer

--- client/src/components/miniplayer/index.tsx
+++ client/src/components/miniplayer/index.tsx
@@ -10,9 +10,12 @@
  */
 
 // #region Imports
-import React from 'react';
 import './style.css'
 import DragableWindow from '../dragable_window';
+
+// import lockedLock from '../../shared/resources/icons/lock-locked.svg?url';
+// import previous from '../../shared/resources/icons/previous.svg';
+// import next from '../../shared/resources/icons/next.svg';
 // #endregion Imports
 // TODO: show and remove animation
 // TODO: add station controls on hover while reserving youtube branding
@@ -22,6 +25,13 @@ function Miniplayer({ youtubeVideoId }: { youtubeVideoId: string }) {
   return (
     <DragableWindow className="miniplayer-container noselect" anchors="SW NW NE SE" resize={true}>
       <iframe id="miniplayer-video" src={`https://www.youtube.com/embed/${youtubeVideoId}?autoplay=1&controls=0&modestbranding=1&mute=1`} />
+      <div className="miniplayer-controls">
+        <div className="miniplayer-controls-center">
+          <i className="icon icon-previous" />
+          <i className="icon icon-lock-locked" />
+          <i className="icon icon-next" />
+        </div>
+      </div>
     </DragableWindow>
   )
 }
